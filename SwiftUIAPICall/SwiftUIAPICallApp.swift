7e19be81ee0eb9a223c558b581f4e8960a24a3c2
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2023-07-18 13:30:22 -0400

    Initial Commit

---
+++ SwiftUIAPICall/SwiftUIAPICallApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAPICallApp.swift
+//  SwiftUIAPICall
+//
+//  Created by Sergey Lukaschuk on 2023-07-18.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAPICallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

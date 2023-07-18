d23de931944adf84195a46bda205862ed853dd45
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2023-07-18 13:56:17 -0400

    Call async

---
+++ SwiftUIAPICall/Course.swift
@@ -0,0 +1,13 @@
+//
+//  Course.swift
+//  SwiftUIAPICall
+//
+//  Created by Sergey Lukaschuk on 2023-07-18.
+//
+
+import Foundation
+
+struct Course: Hashable, Codable {
+    let name: String
+    let image: String
+}
--- SwiftUIAPICall/ViewModel.swift
+++ SwiftUIAPICall/ViewModel.swift
@@ -13,16 +13,31 @@ import Foundation
 
 class ViewModel: ObservableObject {
     
+    @Published var courses: [Course] = []
+    
     func fetch() {
         guard let url = URL(string: "https://iosacademy.io/api/v1/courses/index.php") else {
             return
         }
         
-        let task = URLSession.shared.dataTask(with: url) { data, _, error in
+        let task = URLSession.shared.dataTask(with: url) {[weak self] data, _, error in
             guard let data = data, error == nil else {
                 
                 return
             }
+            
+            do {
+                let courses = try JSONDecoder().decode([Course].self, from: data)
+                
+                DispatchQueue.main.async {
+                    self?.courses = courses
+                }
+            }
+            catch {
+                print(error)
+            }
         }
+        
+        task.resume()
     }
 }

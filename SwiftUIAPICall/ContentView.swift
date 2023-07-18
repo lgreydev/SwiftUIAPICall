4f9a28d1590cdb3440460fc8313af44938720d33
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2023-07-18 14:07:43 -0400

    List update

--- SwiftUIAPICall/ContentView.swift
+++ SwiftUIAPICall/ContentView.swift
@@ -8,12 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @StateObject var viewModel = ViewModel()
+    
     var body: some View {
         NavigationView {
             List {
-                
+                ForEach(viewModel.courses, id: \.self) { course in
+                    HStack {
+                        Image("")
+                            .frame(width: 100, height: 70)
+                            .background(Color.gray)
+                        
+                        Text(course.name)
+                    }
+                    .padding()
+                }
             }
             .navigationTitle("Games News")
+            .onAppear {
+                viewModel.fetch()
+            }
         }
     }
 }
--- SwiftUIAPICall/Course.swift
+++ SwiftUIAPICall/Course.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import SwiftUI
 
 struct Course: Hashable, Codable {
     let name: String
